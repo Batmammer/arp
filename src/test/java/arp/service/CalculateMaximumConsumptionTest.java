c7536769d53421179e1527df3f25559493f05c46
Damian Klata <dklata@kartgis.com.pl>
2021-12-11 08:39:15 +0100

    Algorytm czukania maksymalnego zużycia pojazdów.

--- src/test/java/arp/service/CalculateMaximumConsumptionTest.java
+++ src/test/java/arp/service/CalculateMaximumConsumptionTest.java
@@ -85,4 +85,54 @@ class CalculateMaximumConsumptionTest extends AbstractAlgorithmTest {
         double expectedValue = 0.0;
         assertEquals(expectedValue, value);
     }
+
+    @Test
+    public void shouldUseFullyAccumulator() {
+        // given
+        double storageMaxCapacity = 0.0d;
+
+        Electrolyzer electrolyzer = new Electrolyzer();
+        electrolyzer.maxPower = 2.0d;
+        electrolyzer.efficiency = 1.0d;
+        electrolyzer.accumulatorMaxSize = 4.d;
+        electrolyzer.summaryEnergyProduction = createTableOfValue(0.0);
+        for (int i = 0; i < electrolyzer.summaryEnergyProduction.length; i++) {
+            if (i % 3 == 0) {
+                electrolyzer.summaryEnergyProduction[i] = 6;
+            }
+        }
+        Data data = buildData(electrolyzer, storageMaxCapacity, null);
+
+        // when
+        double value = new CalculateMaximumConsumption(data).calculate();
+
+        // then
+        double expectedValue = 2.0;
+        assertEquals(expectedValue, value);
+    }
+
+    @Test
+    public void shouldUseAccumulatorButWithEnoughCapacity() {
+        // given
+        double storageMaxCapacity = 0.0d;
+
+        Electrolyzer electrolyzer = new Electrolyzer();
+        electrolyzer.maxPower = 2.0d;
+        electrolyzer.efficiency = 1.0d;
+        electrolyzer.accumulatorMaxSize = 3.0d;
+        electrolyzer.summaryEnergyProduction = createTableOfValue(0.0);
+        for (int i = 0; i < electrolyzer.summaryEnergyProduction.length; i++) {
+            if (i % 3 == 0) {
+                electrolyzer.summaryEnergyProduction[i] = 6;
+            }
+        }
+        Data data = buildData(electrolyzer, storageMaxCapacity, null);
+
+        // when
+        double value = new CalculateMaximumConsumption(data).calculate();
+
+        // then
+        double expectedValue = 1.0;
+        assertEquals(expectedValue, value);
+    }
 }
