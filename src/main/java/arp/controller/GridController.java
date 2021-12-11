181355e7791e1460a622d6016f86004e3ba0ba23
Batman <marcinkaczor@gmail.com>
2021-12-11 14:35:09 +0100

    swagger docs

--- src/main/java/arp/controller/GridController.java
+++ src/main/java/arp/controller/GridController.java
@@ -3,6 +3,8 @@ package arp.controller;
 import arp.dto.GridInput;
 import arp.dto.GridResult;
 import arp.service.GridService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,21 +16,23 @@ public class GridController {
         this.gridService = gridService;
     }
 
+    @Operation(summary = "Validate grid given as an input")
     @GetMapping("/validateGrid")
-    public GridResult validateGrid(GridInput gridInput) {
+    public GridResult validateGrid(@Parameter(description = "Input grid") GridInput gridInput) {
         gridService.runSimulation(gridInput);
         return new GridResult();
     }
 
+    @Operation(summary = "Calculate minimal hydrogen production during year")
     @GetMapping("/hydrogenProduction")
-    public GridResult hydrogenProduction(GridInput gridInput) {
+    public GridResult hydrogenProduction(@Parameter(description = "Input grid") GridInput gridInput) {
         gridService.runSimulation(gridInput);
         return new GridResult();
     }
 
-
+    @Operation(summary = "Calculate minimal CAPEX (grid investment cost)")
     @GetMapping("/minCapex")
-    public GridResult minCapex(GridInput gridInput) {
+    public GridResult minCapex(@Parameter(description = "Input grid") GridInput gridInput) {
         gridService.runSimulation(gridInput);
         return new GridResult();
     }
