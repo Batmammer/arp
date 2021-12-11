be7d7f004db9a3b18c07af91d183bfdde4c23c26
Damian Klata <dklata@kartgis.com.pl>
2021-12-11 16:31:07 +0100

    Algorytm czukania maksymalnego zużycia pojazdów.

--- src/test/java/arp/controller/GridControllerTest.java
+++ src/test/java/arp/controller/GridControllerTest.java
@@ -1,13 +1,14 @@
 package arp.controller;
 
 import arp.dto.GridInput;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -15,19 +16,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@JsonTest
 class GridControllerTest {
+    public static final String BASE_DIR = "./src/test/resources/";
+
     @Autowired
     private MockMvc mvc;
 
-    @Autowired
-    private JacksonTester jacksonTester;
 
     @Test
     public void exampleTest() throws Exception {
-        GridInput gridInput = (GridInput)jacksonTester.readObject("validate_grid_in.json");
-        this.mvc.perform(get("/validateGrid", gridInput)).andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
+        File file = new File(BASE_DIR + "validate_grid_in.json");
+        GridInput gridInput = new ObjectMapper().readValue(file, GridInput.class);
+//        this.mvc.perform(get("/validateGrid").param("gridInput", gridInput)).andExpect(status().isOk())
+//                .andExpect(content().string("Hello World"));
     }
 
 }
