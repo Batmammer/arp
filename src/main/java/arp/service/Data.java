ea642f0d6dbef8975c1eea2bc5a553936885114f
Batman <marcinkaczor@gmail.com>
2021-12-11 01:56:20 +0100

    typo

--- src/main/java/arp/service/Data.java
+++ src/main/java/arp/service/Data.java
@@ -6,5 +6,5 @@ import arp.dto.Storage;
 public class Data {
     public GridConstants gridConstants;
     public Storage summaryStorage;
-    public double[] vehiclesConsumpation; // godzina w roku
+    public double[] vehiclesConsumption; // godzina w roku
 }
