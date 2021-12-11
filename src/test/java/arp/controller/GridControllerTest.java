36b109a2e19cfa7154f1fb2e03a716b8c33ecd19
Damian Klata <dklata@kartgis.com.pl>
2021-12-11 17:27:32 +0100

    Algorytm czukania maksymalnego zużycia pojazdów.

--- src/test/java/arp/controller/GridControllerTest.java
+++ src/test/java/arp/controller/GridControllerTest.java
@@ -1,50 +1,60 @@
 package arp.controller;
 
 import arp.dto.GridInput;
+import arp.dto.GridResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.io.File;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebAppConfiguration()
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class GridControllerTest {
     public static final String BASE_DIR = "./src/test/resources/";
 
     @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-    }
+    private MockMvc mvc;
 
     @Test
-    public void exampleTest() throws Exception {
-        File file = new File(BASE_DIR + "validate_grid_in.json");
-        GridInput gridInput = new ObjectMapper().readValue(file, GridInput.class);
-        this.mockMvc.perform(post("/validateGrid", gridInput)).andExpect(status().isOk());
-//                .andExpect(content().string("Hello World"));
+    public void validateGridTest()  {
+        String baseFileName = "validate_grid";
+        String jsonInput = readJsonFromFiles(baseFileName + "_in.json", GridInput.class);
+        String jsonOutput = readJsonFromFiles(baseFileName + "_expected.json", GridResult.class);
+
+        postEndpoint("/validateGrid", jsonInput, jsonOutput);
+    }
+
+    private void postEndpoint(String urlTemplate, String jsonInput, String jsonExpected) {
+        try {
+            this.mvc.perform(post(urlTemplate)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonInput)
+            ).andExpect(status().isOk())
+                    .andExpect(content().string(jsonExpected));
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private <T> String readJsonFromFiles(String fileName, Class<T> jsonClassType) {
+        try {
+            File file = new File(BASE_DIR + fileName);
+            ObjectMapper objectMapper = new ObjectMapper();
+            T gridInput = objectMapper.readValue(file, jsonClassType);
+            String jsonInput = objectMapper.writeValueAsString(gridInput);
+            return jsonInput;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
 }
