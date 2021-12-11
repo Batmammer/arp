71e0af17517b5ab92e6fcc6d5c361860bbe6a3fc
Batman <marcinkaczor@gmail.com>
2021-12-11 13:34:03 +0100

    big refactor

--- src/main/java/arp/service/Data.java
+++ src/main/java/arp/service/Data.java
@@ -1,6 +1,7 @@
 package arp.service;
 
 import arp.dto.GridConstants;
+import arp.dto.GridCosts;
 import arp.dto.grid.Storage;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import java.util.Map;
 @NoArgsConstructor
 public class Data {
     public GridConstants gridConstants;
+    public GridCosts gridCosts;
     public List<Storage> storages;
     public double[] vehiclesConsumption; // godzina w roku
 
--- src/main/java/arp/service/GridService.java
+++ src/main/java/arp/service/GridService.java
@@ -1,10 +1,19 @@
 package arp.service;
 
 import arp.dto.*;
+import arp.dto.grid.Electrolyzer;
+import arp.dto.grid.EnergySource;
+import arp.dto.grid.Storage;
+import arp.dto.grid.Vehicle;
+import arp.dto.util.WeeklyPeriod;
 import arp.enums.EnergySourceType;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -13,27 +22,22 @@ public class GridService {
     private CalculateYearAlgorithm calculateYearAlgorithm;
 
     public YearResult runSimulation(GridInput gridInput) {
-        calculateYearAlgorithm = new CalculateYearAlgorithm(new Data(gridInput.getConstants(),
-                new Storage(gridInput.getGrid().getWatSide().getStorage().getMaxCapacity(),
-                        calculateElectrolyzers(
-                                gridInput.getGrid().getWatSide().getStorage().getElectrolyzers(),
-                                gridInput.getConstants().getTransmissionLoss())),
-                calculateYearlyConsumption(gridInput.getGrid().getHydrogenSide(),
-                        gridInput.getConstants().getHydrogenTransportLoss())));
+        calculateYearAlgorithm = new CalculateYearAlgorithm(
+                new Data(
+                        gridInput.getConstants(),
+                        gridInput.getCosts(),
+                        gridInput.getGrid().getStorages(),
+                calculateYearlyConsumption(gridInput.getGrid().getVehicles(),
+                        gridInput.getConstants().getHydrogenTransportLoss()),
+                calculateElectrolyzers(gridInput.getGrid().getStorages().stream()
+                        .map(s -> s.getElectrolyzers()).collect(Collectors.toList()).stream().flatMap(List::stream).collect(Collectors.toList()),
+                        gridInput.getConstants().getTransmissionLoss())
+                );
         return calculateYearAlgorithm.calculate();
     }
 
-    private List<Electrolyzer> calculateElectrolyzers(List<Electrolyzer> input, double transmissionLoss) {
-        return input.stream().map(e -> {
-            Electrolyzer electrolyzer =new Electrolyzer();
-            electrolyzer.setEfficiency(e.getEfficiency());
-            electrolyzer.setSources(e.getSources());
-            electrolyzer.setAccumulatorMaxSize(e.getAccumulatorMaxSize());
-            electrolyzer.setMinPower(e.getMinPower());
-            electrolyzer.setMaxPower(e.getMaxPower());
-            electrolyzer.setSummaryEnergyProduction(calculateSummaryEnergyProduction(e, transmissionLoss));
-            return electrolyzer;
-        }).collect(Collectors.toList());
+    private Map<Long, double[]> calculateElectrolyzers(List<Electrolyzer> input, double transmissionLoss) {
+        return input.stream().collect(Collectors.toMap(Electrolyzer::getId, e -> calculateSummaryEnergyProduction(e, transmissionLoss));
     }
 
     private double[] calculateSummaryEnergyProduction(Electrolyzer electrolyzer, double transmissionLoss) {
@@ -52,17 +56,39 @@ public class GridService {
         return 1.0;
     }
 
-    private double[] calculateYearlyConsumption(HydrogenSide hydrogenSide, Double hydrogenTransportLoss) {
+    private double[] calculateYearlyConsumption(List<Vehicle> vehicles, Double hydrogenTransportLoss) {
         double[] consumption = new double[365 * 24];
+        Map<Long, boolean[]> weeklyWork = new HashMap<>();
+        for (Vehicle v : vehicles) {
+            weeklyWork.put(v.getId(), calculateWeekly(v.getWeeklyWork()));
+        }
         for (int i = 0; i < 365 * 24; i++) {
             double current = 0;
-            for (Vehicle v : hydrogenSide.getVehicles()) {
-                if (v.getWeeklyWork()[i % (24 * 7)]) {
+            for (Vehicle v : vehicles) {
+                if (weeklyWork.get(v.getId())[i % (24 * 7)]) {
                     current += v.getCount() * v.getFuelConsumption() + v.getDistance() * hydrogenTransportLoss;
                 }
             }
             consumption[i] = current;
         }
         return consumption;
+    }
+
+    private boolean[] calculateWeekly(List<WeeklyPeriod> periods) {
+        boolean[] weekly = new boolean[24 * 7];
+        Arrays.fill(weekly, false);
+        for (WeeklyPeriod p : periods) {
+           if (p.getDayFrom() < 0 || p.getDayTo() > 6 || p.getDayFrom() > p.getDayTo())
+               continue;
+            if (p.getHourFrom() < 0 || p.getHourTo() > 23 || p.getHourFrom() > p.getHourTo())
+                continue;
+            for(int i = p.getDayFrom(); i <= p.getDayTo(); i++) {
+                for (int j = p.getHourFrom(); j <= p.getHourTo(); j++) {
+                    weekly[i * 7 + j] = true;
+                }
+            }
+
+        }
+        return weekly;
     }
 }
