ae74cb5e483009406953d97e639830e6b964d547
Batman <marcinkaczor@gmail.com>
2021-12-11 15:57:38 +0100

    controller fix

--- src/main/java/arp/service/Utils.java
+++ src/main/java/arp/service/Utils.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 
 public class Utils {
     public static final int HOURS_OF_YEAR = 24 * 365;
-    private static double pvMultiplier[] = {
-    };
-    private static double windMultiplier[] = {
-    };
 
     public static double[] createTableOfValue(double value) {
         double[] table = new double[HOURS_OF_YEAR];
@@ -19,11 +15,4 @@ public class Utils {
         return "" + Math.round(1000 * d) / 1000.0;
     }
 
-    public static double getPvMultiplier(int index) {
-        return pvMultiplier[index];
-    }
-
-    public static double getWindMultiplier(int index) {
-        return windMultiplier[index];
-    }
 }
